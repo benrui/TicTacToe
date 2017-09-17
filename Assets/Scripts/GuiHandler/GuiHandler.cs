2d7d02b6ff8e4bd67a8eb8d52fc1665d41bd5ada
Ismail Simsek <ismailsimsek09@gmail.com>
2017-09-18 00:40:13 +0200

    gui handler will handle lobby and game grid

--- Assets/Scripts/GuiHandler/GuiHandler.cs
+++ Assets/Scripts/GuiHandler/GuiHandler.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Zenject;
 
 public class GuiHandler : MonoBehaviour {
+    public GameObject Lobby;
+    public GameObject GameGrid;
+    
     private GameStateManager stateManager;
     private IPopupManager popupManager;
     private INetworkService networkService;
@@ -12,12 +13,19 @@ public class GuiHandler : MonoBehaviour {
     public void Construct(
         GameStateManager stateManager,
         IPopupManager popupManager,
-        INetworkService networkService
+        INetworkService networkService,
+        StartGameSignal startGameSignal
     )
     {
         this.stateManager = stateManager;
         this.popupManager = popupManager;
         this.networkService = networkService;
+        startGameSignal += OnStartGame;
+    }
+
+    private void OnStartGame(Opponent obj) {
+        Lobby.SetActive(false);
+        GameGrid.SetActive(true);
     }
 
     public void StartNewGame() {
