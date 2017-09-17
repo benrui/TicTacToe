b77075a6ae4e9ed7d377f581651ea34a12533fcc
Ismail Simsek <ismailsimsek09@gmail.com>
2017-09-18 00:41:30 +0200

    playing state added

---
+++ Assets/Scripts/Core/States/PlayingState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayingState : IGameState {
+    public void EnterState() {
+        Debug.Log("prepare game ui");
+    }
+
+    public void ExitState() {
+        Debug.Log("dispose game ui or hide");
+    }
+
+    public void Update() {
+        //
+    }
+
+    public void FixedUpdate() {
+        //
+    }
+
+    [Serializable]
+    public class Settings {
+        //public GameGrid gameGrid;
+    }
+}
