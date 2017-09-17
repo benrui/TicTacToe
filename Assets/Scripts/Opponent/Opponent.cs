86733b74033d1ac55d9b1b3fd81d3be8d151a85b
Ismail Simsek <ismailsimsek09@gmail.com>
2017-09-18 00:36:53 +0200

    Opponent VO created

---
+++ Assets/Scripts/Opponent/Opponent.cs
@@ -0,0 +1,9 @@
+﻿public class Opponent {
+    private readonly string uid;
+    private readonly string name;
+
+    public Opponent(string uid, string name) {
+        this.uid = uid;
+        this.name = name;
+    }
+}
